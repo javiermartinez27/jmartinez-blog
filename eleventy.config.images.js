cae285b5fd3efb8698823031346fecdd7c6c2e80
Javier <javier.martinezsa@sansano.usm.cl>
2024-08-27 09:47:41 -0400

    chore: desktop postlist first rows done

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -12,7 +12,7 @@ module.exports = eleventyConfig => {
 
 	// Eleventy Image shortcode
 	// https://www.11ty.dev/docs/plugins/image/
-	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes) {
+	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes, showText = true) {
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
 		let formats = ["webp"];
@@ -43,9 +43,16 @@ module.exports = eleventyConfig => {
 		let dimensions = sizeOf(file);
 		let width = widths ? widths[0] : dimensions.width;
 		let height = widths ? widths[0] * dimensions.height / dimensions.width : dimensions.height;
-		return "<style scoped>." + fileNameWithoutExtension + "{width:" + width + "px; height:" + height + "px;}</style>" +
-			"<div style='display: flex; justify-content: center;'>" +
-			eleventyImage.generateHTML(metadata, imageAttributes) + "</div>" + 
-			"<p style='font-size: 12px; display: flex; justify-content: center; margin-bottom: 5rem'>" + alt + "</p>";
+
+		if(showText) {
+			return "<style scoped>." + fileNameWithoutExtension + "{width:" + width + "px; height:" + height + "px;}</style>" +
+				"<div style='display: flex; justify-content: center;'>" +
+				eleventyImage.generateHTML(metadata, imageAttributes) + "</div>" + 
+				"<p style='font-size: 12px; display: flex; justify-content: center; margin-bottom: 5rem'>" + alt + "</p>";
+		} else {
+			return "<style scoped>." + fileNameWithoutExtension + "{width:" + width + "px; height:" + height + "px;}</style>" +
+				"<div style='display: flex; justify-content: center;'>" +
+				eleventyImage.generateHTML(metadata, imageAttributes) + "</div>"
+		}
 	});
 };
